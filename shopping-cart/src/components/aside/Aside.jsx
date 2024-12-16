c8dbb37826bf6f5e749ff705ab25e6ce8f3df446
Cairon29 <josewjjunco@gmail.com>
2024-12-15 23:57:07 -0500

    add filter context to the app

--- shopping-cart/src/components/aside/Aside.jsx
+++ shopping-cart/src/components/aside/Aside.jsx
@@ -1,15 +1,36 @@
+import { useContext, useState } from "react"
+import { FilterContext } from "../../contexts/filters";
 
 export const Aside = () => {
+  const [range, setRange] = useState(0);
+
+  const { filter, setFilter} = useContext(FilterContext);
+
+  const handleRange = (e) => {
+    setFilter(prev => ({
+      ...prev,
+      minPrice: e.target.value
+    }))
+    console.log(filter.minPrice);
+    
+  }
+
   return (
     <aside>
+      <div>
         <label htmlFor="electronics"> Electronics</label>
         <input name='electronics' type="checkbox"/>
+      </div>
+      <div>
         <label htmlFor="books"> Books</label>
         <input name='books' type="checkbox"/>
+      </div>
+      <div>
         <label htmlFor="clothing"> Clothing</label>
         <input name='clothing' type="checkbox"/>
-
-        <input type="range" min="0" max="5000"/>
+      </div>
+        <label htmlFor="minRange">Minimun range: {filter.minPrice}</label>
+        <input name="minRange" type="range" min="0" max="1000" onChange={handleRange}/>
     </aside>
   ) 
 }
--- shopping-cart/src/main.jsx
+++ shopping-cart/src/main.jsx
@@ -1,10 +1,10 @@
-import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { FilterProvider } from './contexts/filters.jsx'
 import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
+  <FilterProvider>
     <App />
-  </StrictMode>,
+  </FilterProvider>
 )
