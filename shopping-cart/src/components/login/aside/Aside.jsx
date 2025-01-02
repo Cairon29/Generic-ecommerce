32d2c166e69330ce790e876e0b9c95d5177c403c
Cairon29 <josewjjunco@gmail.com>
2025-01-01 23:53:32 -0500

    minor: reduntant css eliminated

--- shopping-cart/src/components/login/aside/Aside.jsx
+++ shopping-cart/src/components/login/aside/Aside.jsx
@@ -2,7 +2,7 @@
 export const Aside = () => {
   return (
     <aside id="aside-login">
-        <img src="../../../../public/login.png" alt="log-in image" width="200px" />
+        <img src="../../../../public/login.png" alt="log-in" width="200px" />
         <div>
             <h2> Lets get started!</h2>
             <p> Identify yourself to find products and items you love in this site! </p>
